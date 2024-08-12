edb28b02e99c40406a6250663ce62ad93baa63a8
Zala Gregorc <gregorc.zala@gmail.com>
2024-08-12 23:24:11 +0200

    odstranila neuporabne funkcije in dodala funkcijo, ki shrani besedilo v datoteke

--- prevedi_podatke.py
+++ prevedi_podatke.py
@@ -1,26 +1,27 @@
 import requests # ENOSTAVNEJŠA OD URLLIB
-import re
-import os
+import re # za uporabo regularnih izrazov
+import os 
 
-def prevedi_v_niz(url):
+def prevedi_v_niz(link):
     try:
-        besedilo = requests.get(url).text
+        besedilo = requests.get(link).text
     except requests.exceptions.RequestException:
         print("Prišlo je do napake!")
         return None
     return besedilo
 
-def shrani_besedilo(besedilo, datoteka):
-    
+def shrani_v_datoteko(text, mapa, file):
+    """ 
+    Funkcija skrani podatke (text) 
+    """
+    # ustvarimo mapo, če ta še ne obstaja
+    os.makedirs(mapa, exist_ok=True)
+    # path nam pove, kje lahko našo datoteko najdemo ("pot do datoteke")
+    path = os.path.join(mapa, file)
+    # besedlo zapišemo v (novo) datoteko
+    with open(path, 'w', encoding='utf-8') as izhodna:
+        izhodna.write(text)
     return None
 
-# KOPIRANI IZ ZAPISKOV!!
-def vse_pojavitve(besedilo: str, iskani_niz: str):
-    konec_pojavitve = 0
-    while True:
-        try:
-            zacetek_pojavitve = besedilo.index(iskani_niz, konec_pojavitve)
-            konec_pojavitve = min(zacetek_pojavitve + len(iskani_niz), len(besedilo))
-            yield zacetek_pojavitve, konec_pojavitve
-        except ValueError:
-            break
+
+
