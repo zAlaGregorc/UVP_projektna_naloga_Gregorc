116d0459b98c842bc510005f85afc482566e6c4b
Zala Gregorc <gregorc.zala@gmail.com>
2024-08-21 23:39:17 +0200

    delujoča skripta za novo spletno stran

--- prevedi_in_shrani_podatke.py
+++ prevedi_in_shrani_podatke.py
@@ -1,8 +1,7 @@
 import requests # za pošiljanje zahtevkov in prejemanje odgovorov s spletnih strežnikov 
-import os 
-import re
+import os # za ustvarjanje ali popravljanje map/datotek
+import re # za regularne izraze
 
-print("Skripta se je začela izvajati.")
 
 def prevedi_v_niz(url):
     """
@@ -21,6 +20,8 @@ def prevedi_v_niz(url):
     # Če ni prišlo do napake funkcija vrne vsebino spletne strani.
     print("Vsebina je prevedena")
     return vsebina
+
+
 
 def shrani_v_datoteko(besedilo, mapa, datoteka):
     """ 
@@ -41,35 +42,20 @@ def shrani_v_datoteko(besedilo, mapa, datoteka):
     except OSError:
         print(f"Napaka pri shranjevanju")
         return False
-
-def shrani_glavno_stran_v_datoteko(url, zvrsti):
-    """
-    Ta funkcija nalaga strani iz katerih bomo dobili podatke 
-    """
-    for zvrst in zvrsti:
-        # prenesli bomo le 8 strani podatkov, zato for zanko zapišemo v razponu od 1 do 8 
-        for i in range(1, 9):
-            novi_url = url + "/knjige/" + zvrst + f"/stran-{i}/"
-            print(f"Pridobivam: {novi_url}")
-            besedilo = prevedi_v_niz(novi_url)
-            if besedilo:
-                shrani_v_datoteko(besedilo, f"podatki/{zvrst}", f"{i}.html")
-            else:
-                print(f"Strani ni bilo mogoče prenesti: {novi_url}")
-            
-
-# Definirajmo nekaj spremenljivk, ki jih potrebujemo.
-# url glavne spletne strani - v mojem primeru nepremičnine.net
-url = "https://beletrina.si"
-# Imeni map, kamor bomo shranili naše podatke; zapišemo v obliki seznama vrste.
-zvrsti = ["roman", "poezija"]
-shrani_glavno_stran_v_datoteko(url, zvrsti)
+    
+ 
 
 def izlusci_pot_do_knjig(html):
-    naslov_knjige_vzorec = r"<a href=\"(.+)\" class=\""
-    naslov_knjige = re.findall(naslov_knjige_vzorec, html, flags=re.DOTALL)
-    return naslov_knjige if naslov_knjige else []
-    
+    """
+    Funkcija poišče vse dopolnitve url-ja za dostop do spletnih strani knjig. Le-te shrani v seznam in ga vrne.
+    """
+    # napišemo vzorec po katerem najdemo del linka v html datoteki
+    naslov_knjige_vzorec = r'<a href="(/knjiga/.+?)" class="'
+    # poiščemo vse pojavitve
+    naslov_knjige = re.findall(naslov_knjige_vzorec, html)
+    return naslov_knjige
+
+
 
 def pridobi_podatke_iz_datoteke(datoteka):
     """
@@ -77,13 +63,10 @@ def pridobi_podatke_iz_datoteke(datoteka):
     """
     with open(datoteka, "r", encoding='utf-8') as f:
         vsebina = f.read()
-        najdene_povezave_do_knjig = izlusci_pot_do_knjig(vsebina)
-        # Preverimo, če je najdena povezava
-        if najdene_povezave_do_knjig:
-            return najdene_povezave_do_knjig
-        else:
-            return None
-       
+        return izlusci_pot_do_knjig(vsebina)
+    
+
+
 def vsi_linki_knjig(mapa):
     """
     Funkcija pridobi vse podatke iz vseh html datotek v mapi.
@@ -100,21 +83,28 @@ def vsi_linki_knjig(mapa):
                 print(f"Napaka pri obdelavi datoteke {pot_do_datoteke}: {e}")
     return link_knjige
 
+
+
 def shrani_stran_knjige(url, mapa, ime_datoteke):
+    """
+    Funkcija shrani html zapise v datoteko "ime_datoteke", katero najdemo v mapi z imenom "mapa". 
+    """
     besedilo = prevedi_v_niz(url)
     if besedilo:
         shrani_v_datoteko(besedilo, mapa, ime_datoteke)
     else:
         print(f"Strani ni bilo mogoče prenesti: {url}")
 
+
+
 def main():
     url = "https://beletrina.si"
-    
-    linki = vsi_linki_knjig("podatki/" + "roman")
+    linki = vsi_linki_knjig("podatki/roman")
     for link in linki:
         novi_url = url + link
-        shrani_stran_knjige(novi_url, f"podatki/roman/knjige", f"{link.split('/')[-1]}.html")
+        print(f"Pridobivam: {novi_url}")
+        shrani_stran_knjige(novi_url, "podatki/roman/knjige", f"{link.split('/')[-1]}.html")
+
 
 if __name__ == "__main__":
     main()
-            
