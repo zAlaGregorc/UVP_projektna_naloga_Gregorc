47cbaeb6237b72fe72a8106800d45f570d2b203b
Zala Gregorc <gregorc.zala@gmail.com>
2024-08-22 22:36:13 +0200

    izboljšana koda za pridobivanje info. iz več kate.

--- obdelaj_podatke.py
+++ obdelaj_podatke.py
@@ -1,42 +1,42 @@
 import re
 import os
-import csv
+import csv #
 
 
-
-class Nepremicnine():
+class Knjige():
     """ 
-    Razred Nepremicnine predstavlja eno nepremicnino, ki se prodaja. Vsaka nepremicnina ima napisano točno 
-    lokacijo, tip nepremičnine, kvadraturo in ceno, za katero se prodaja.
+    Razred Knjige predstavlja eno knjigo, ki se prodaja. Vsaka knjiga ima za atribute 
+    naslov knjige, avtorja knjige, število strani (dolžina), cena knjige (mehka vezava),
+    cena elektronske knjige (ecena).
     """
 
-    def __init__(self, lokacija, tip, kvadratura, cena):
-        # Nastavimo začetne vrednosti za podatke, ki nas zanimajo.
-        self.lokacija = lokacija
-        self.tip = tip
-        self.kvadratura = kvadratura
+    def __init__(self, naslov, avtor, dolzina, cena, ecena):
+        # Nastavimo začetne vrednosti za podatke.
+        self.lokacija = naslov
+        self.tip = avtor
+        self.kvadratura = dolzina
         self.cena = cena
-        self.vrsta_nepremicnine = None
-        self.obmocje = None 
+        self.ecena = ecena
+        self.zvrst = None
 
     def __str__(self):
         """
-        Vrnemo niz, ki opisuje nepremičnino.
+        Vrnemo niz, ki opisuje eno knjigo.
         """
-        return f"Nepremičnina - lokacija: {self.lokacija},\n tip nepremičnine: {self.tip},\n kvadratura: {self.kvadratura} m² in\n cena: {self.cena} €"
+        return f"Knjiga z naslovom {self.naslov},\n avtor: {self.avtor},\n dolžina: {self.dolzina} strani,\n cena mehke vezave: {self.cena} € in\n cena e-knjige: {self.ecena}."
     
     def __repr__(self):
         """
-        Funkcija vrne opis nepremičnine (objekta), ki je primeren za razhroščevanje, shranjevanje 
+        Funkcija vrne opis knjige (objekta), ki je primeren za razhroščevanje, shranjevanje 
         in ponovno ustvarjanje objekta.
         """
-        return f"Nepremicnine(lokacija='{self.lokacija}', tip='{self.tip}', kvadratura={self.kvadratura}, cena={self.cena:.2f})"
+        return f"Nepremicnine(lokacija='{self.naslov}', tip='{self.avtor}', kvadratura={self.dolzina}, cena mehke vezave={self.cena:.2f}, cena e-knjige={self.ecena:.2f})"
 
     def pretvori_v_slovar(self):
-        '''
+        """
         Vrne slovar, ki vsebuje podatke nepremičnine. Kasneje ga bomo zapisali v csv.
-        '''
-        return {"lokacija": self.lokacija, "tip nepremičnine": self.tip, "kvadratura": self.kvadratura, "cena": self.cena}
+        """
+        return {"naslov": self.naslov, "avtor": self.avtor, "dolzina": self.dolzina, "cena": self.cena, "ecena": self.ecena}
     
 print("Razred je narejen.")   
     
@@ -44,51 +44,39 @@ def izlusci_podatke(html):
     """
     Funkcija izlušči vse podatke iz html zapisa, ki jih potrebujemo za analizo.
     """
-    # 1. LOKACIJA
+    # 1. NASLOV
     # Za vsak podatek posebej najprej napišemo vzorec z regularnimi izrazi, po katerem najdemo podatek v html-ju.
-    lokacija_vzorec = r"itemprop=\"name\" title=\"(\w+)"
+    naslov_vzorec = r"<div class=\"title-02\">\s+(\w+)\s+<\/div>"
     # Te vzorce poiščemo v html-ju.
-    lokacija = re.search(lokacija_vzorec, html, flags=re.DOTALL)
+    naslov = re.search(naslov_vzorec, html, flags=re.DOTALL)
     
-    # 2. TIP NEPREMIČNINE
-    tip_vzorec = r"Prodaja: (.+,).+\"tipi\">(.*?)<"
-    tip = re.findall(tip_vzorec, html, flags=re.DOTALL)
     
-    # 3. CENA
-    cena_vzorec = r">([\d\.]+,\d+\s?[€$£])<"
-    cena = re.search(cena_vzorec, html, flags=re.DOTALL)
+    # 2. AVTOR
+    avtor_vzorec = r"class=\"link-underline\">(.+?)<\/a>"
+    avtor = re.findall(avtor_vzorec, html, flags=re.DOTALL)
+    
+    
+    # 3. ŠTEVILO STRANI
+    dolzina_vzorec = r"<dt>Št\. strani<\/dt>\s*<dd>(\d+)<\/dd>"
+    dolzina = re.search(dolzina_vzorec, html, flags=re.DOTALL) + "strani"
+    
+    
+    # 4. CENA MEHKE VEZAVE IN E-KNJIGE
+    cena_vzorec = r"<div class=\"title-06 \">(\d+,\d+ €)<\/div>"
+    cene = re.findall(cena_vzorec, html, flags=re.DOTALL) + "€"
     # Zapišemo, kaj se zgodi, če cena ni podana.
     if cena != None:
-        cena = cena.group()
+        cena = cene[0]
+        ecena = cene[1]
     else:
-        # če oglas nima cene, ga filtriramo
+        # če knjiga nima cene, jo filtriramo
         return None
     
-    # 4. KVADRATURA
-    kvadratura_vzorec = r"(\d+\,\d+) m"
-    kvadratura = re.search(kvadratura_vzorec, html, flags=re.DOTALL) + "m²"
-    
-    return Nepremicnine(lokacija, tip, kvadratura, cena)
+    return Knjige(naslov, avtor, dolzina, cena, ecena)
 
 print("Podatki so izluščeni.")
 
-def izlusci_oglase(vsebina):
-    """ 
-    Ta funckija iz html datoteke potegne bloke, ki predstavljajo posamezen oglas za nepremičnino. Vrne seznam oglasov 
-    oziroma podatke iz oglasov, ki jih želimo.
-    """
-    # Napišemo najprej vzorec po katerem najdemo blok.
-    blok_vzorec = r"<div class=\"col-md-6 col-md-12 position-relative\">"
-    # Te bloke (vzorce) poiščemo v našem html-ju.
-    nepremicnine_neobdelano = re.findall(blok_vzorec, vsebina, flags=re.DOTALL)
-    # Vse oglase (bloke) shranimo v seznam.
-    nepremicnine = []
-    # Napišemo for zanko, ki za vsako nepremičnino izlušči podatke, ki jih doda v seznam, če že niso v seznamu.
-    for nepremicnina in nepremicnine_neobdelano:
-        prebran_oglas = izlusci_podatke(nepremicnina)
-        if prebran_oglas != None:
-            nepremicnine.append(prebran_oglas)
-    return nepremicnine
+#!!!!!!!!!!!!!
 
 
 def pridobi_podatke_iz_datoteke(datoteka):
@@ -97,24 +85,23 @@ def pridobi_podatke_iz_datoteke(datoteka):
     """
     with open(datoteka, "r", encoding='utf-8') as f:
         vsebina = f.read()
-        najdene_nepremicnine = izlusci_oglase(vsebina)
-        return najdene_nepremicnine
+        return izlusci_podatke(vsebina)
 
-def vsi_oglasi_nepremicnin(mapa):
+def vse_knjige(mapa):
     """
     Funkcija pridobi vse podatke iz vseh html datotek v mapi.
     """
-    nepremicnine = []
+    knjiga = []
     for datoteka in os.listdir(mapa):
         if datoteka.endswith(".html"):
             pot_do_datoteke = os.path.join(mapa, datoteka)
             try:
-                nepremicnine.extend(pridobi_podatke_iz_datoteke(pot_do_datoteke))
+                knjige.extend(pridobi_podatke_iz_datoteke(pot_do_datoteke))
             except FileNotFoundError:
                 print(f"Datoteka {pot_do_datoteke} ni najdena.")
             except Exception as e:
                 print(f"Napaka pri obdelavi datoteke {pot_do_datoteke}: {e}")
-    return nepremicnine
+    return Knjige
             
 
 def write_csv(fieldnames, rows, directory, filename):
@@ -131,24 +118,21 @@ def write_csv(fieldnames, rows, directory, filename):
             writer.writerow(row)
     return
 
-def pripravi_podatke_za_csv():
-    '''
-    Funkcija
-    '''
-    vrsta_nepremicnin = ["stanovanje", "hisa"]
-    obmocja = ["ljubljana-mesto", "ljubljana-okolica", "posavje", "koroska"]
-    vsi_podatki = []
-    for obmocje in obmocja:
-        for vrsta_nepremicnine in vrsta_nepremicnin:
-            oglasi = vsi_oglasi_nepremicnin("podatki/" + obmocje + "/" + vrsta_nepremicnine)
-
-            for oglas in oglasi:
-                oglas.vrsta_nepremicnine = vrsta_nepremicnine
-                oglas.obmocje = obmocje         
-                vsi_podatki.extend(oglasi)
-                return vsi_podatki
+# def pripravi_podatke_za_csv():
+#     '''
+#     Funkcija
+#     '''
+#     vrsta_nepremicnin = ["stanovanje", "hisa"]
+#     obmocja = ["ljubljana-mesto", "ljubljana-okolica", "posavje", "koroska"]
+#     vsi_podatki = []
+#     for knjiga in knjige:
+#         knjiga.vrsta_nepremicnine = vrsta_nepremicnine
+#         knjiga.obmocje = obmocje         
+#         vsi_podatki.extend(oglasi)
+#         return vsi_podatki
+    
 vsi_podatki = pripravi_podatke_za_csv()
     
                 
-fieldnames = ["LOKACIJA NEPREMIČNINE", "TIP NEPREMIČNINE", "KVADRATURA NEPREMIČNINE", "CENA NEPREMIČNINE"]
-write_csv(fieldnames, vsi_podatki, "koncni_podatki", "nepremicnine.csv")
+fieldnames = ["NASLOV", "AVTOR", "ŠTEVILO STRANI", "CENA MEHKE VEZAVE", "CENA E-KNJIGE"]
+write_csv(fieldnames, vsi_podatki, "koncni_podatki", "knjige.csv")
